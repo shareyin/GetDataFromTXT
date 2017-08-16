bd00341e0885fbb35d996730d90079b15f45c49d
shareyin <shareyin@qq.com>
2017-08-16 10:29:02 +0800

    提取公里标数据
    从株车数据中提取公里标数据

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace HanderTxt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(173, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(251, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(78, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "地面数据目录";
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(80, 63);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 2;
+            this.btnGo.Text = "走你";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(80, 92);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(443, 155);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(436, 253);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 27);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "导出Excel";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(430, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "选择";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 336);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace HanderTxt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            string path = textBox1.Text;
+            ReadText(path, 0);
+            
+        }
+
+        public void ReadText(string path,int a)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("站点名", typeof(String));
+            dt.Columns.Add("信号机", typeof(String));
+            dt.Columns.Add("信号闭塞", typeof(String));
+            dt.Columns.Add("制式", typeof(String));
+            dt.Columns.Add("公里标", typeof(String));
+            dt.Columns.Add("距离", typeof(String));
+            dt.Columns.Add("分区", typeof(String));
+            dt.Columns.Add("分区限速", typeof(String));
+            dt.Columns.Add("黄灯", typeof(String));
+            dt.Columns.Add("黄灯限速", typeof(String));
+            dt.Columns.Add("校正", typeof(String));
+            dt.Columns.Add("有权", typeof(String));
+            string[] str = new string[1024];
+            ExcelCreate excelc = new ExcelCreate();
+            try 
+            {
+                using (StreamReader sr = new StreamReader(path, GetEncoding(path, UnicodeEncoding.Default)))
+                {
+                    string line;
+                    int index1 = 0, index2 = 0, index3 = 0;
+                    string lineState="";
+                    string lineSignal="";
+                    string lineBisai = "";
+                    string lineZhishi = "";
+                    string lineGonglibiao = "";
+                    string lineJuli = "";
+                    string lineFenqu = "";
+                    string lineFenxian = "";
+                    string lineHuangdeng = "";
+                    string lineHuangxian = "";
+                    string lineJiaozheng = "";
+                    string lineYouquan = "";
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        if (line.IndexOf("闭") > 0)
+                        {
+                            index2 = line.Length;
+                            index1 = line.IndexOf("]");
+                            index3 = line.IndexOf("闭");
+                            lineYouquan = line.Substring(index2 - 2, 2);
+                            lineJiaozheng = line.Substring(index2 - 5, 2);
+                            lineHuangxian = line.Substring(index2 - 11, 3);
+                            lineHuangdeng = line.Substring(index2 - 15, 3);
+                            lineFenxian = line.Substring(index2 - 19, 3);
+                            lineFenqu = line.Substring(index2 - 23, 3);
+                            lineGonglibiao = line.Substring(index2 - 49, 8);
+                            lineZhishi = line.Substring(index2 - 52, 2);
+                            lineBisai = line.Substring(index3 - 1, 2);
+                            lineSignal = line.Substring(index3 - 9, 7);
+                            lineState = line.Substring(2, 10);
+                            DataRow dr = dt.NewRow();
+                            dr[0] = lineState;
+                            dr[1] = lineSignal;
+                            dr[2] = lineBisai;
+                            dr[3] = lineZhishi;
+                            dr[4] = lineGonglibiao;
+                            dr[5] = lineJuli;
+                            dr[6] = lineFenqu;
+                            dr[7] = lineFenxian;
+                            dr[8] = lineHuangdeng;
+                            dr[9] = lineHuangxian;
+                            dr[10] = lineJiaozheng;
+                            dr[11] = lineYouquan;
+                            dt.Rows.Add(dr);
+                        }
+                    }
+                }
+                string OutPath = string.Format("{0}\\{1}", Application.StartupPath, DateTime.Now.ToString("yyyyMMddHHmmss")+"OUT.xls");
+                excelc.OutFileToDisk(dt, "导出数据表", OutPath);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        //protected void ExportExcel(DataTable dt)
+        //{
+        //    if (dt == null || dt.Rows.Count == 0) return;
+        //    Microsoft.Office.Interop.Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+
+        //    if (xlApp == null)
+        //    {
+        //        return;
+        //    }
+        //    System.Globalization.CultureInfo CurrentCI = System.Threading.Thread.CurrentThread.CurrentCulture;
+        //    System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+        //    Microsoft.Office.Interop.Excel.Workbooks workbooks = xlApp.Workbooks;
+        //    Microsoft.Office.Interop.Excel.Workbook workbook = workbooks.Add(Microsoft.Office.Interop.Excel.XlWBATemplate.xlWBATWorksheet);
+        //    Microsoft.Office.Interop.Excel.Worksheet worksheet = (Microsoft.Office.Interop.Excel.Worksheet)workbook.Worksheets[1];
+        //    Microsoft.Office.Interop.Excel.Range range;
+        //    long totalCount = dt.Rows.Count;
+        //    long rowRead = 0;
+        //    float percent = 0;
+        //    for (int i = 0; i < dt.Columns.Count; i++)
+        //    {
+        //        worksheet.Cells[1, i + 1] = dt.Columns[i].ColumnName;
+        //        range = (Microsoft.Office.Interop.Excel.Range)worksheet.Cells[1, i + 1];
+        //        range.Interior.ColorIndex = 15;
+        //        range.Font.Bold = true;
+        //    }
+        //    for (int r = 0; r < dt.Rows.Count; r++)
+        //    {
+        //        for (int i = 0; i < dt.Columns.Count; i++)
+        //        {
+        //            worksheet.Cells[r + 2, i + 1] = dt.Rows[r][i].ToString();
+        //        }
+        //        rowRead++;
+        //        percent = ((float)(100 * rowRead)) / totalCount;
+        //    }
+        //    xlApp.Visible = true;
+        //}
+        public Encoding GetEncoding(string fileName, Encoding defualencoding)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Open);
+            Encoding tagetEncoding = GetEncoding(fs, defualencoding);
+            fs.Close();
+            return tagetEncoding;
+        }
+
+        public Encoding GetEncoding(FileStream stream, Encoding defualEncoding)
+        {
+            Encoding tagetEncoding = defualEncoding;
+            if (stream != null && stream.Length >= 2)
+            {
+                byte byte1 = 0;
+                byte byte2 = 0;
+                byte byte3 = 0;
+                byte byte4 = 0;
+                long origPos = stream.Seek(0, SeekOrigin.Begin);
+                stream.Seek(0, SeekOrigin.Begin);
+
+                int nByte = stream.ReadByte();
+                byte1 = Convert.ToByte(nByte);
+                byte2 = Convert.ToByte(stream.ReadByte());
+                if (stream.Length >= 3)
+                {
+                    byte3 = Convert.ToByte(stream.ReadByte());
+                }
+                if (stream.Length >= 4)
+                {
+                    byte4 = Convert.ToByte(stream.ReadByte());
+                }
+                if (byte1 == 0xFE && byte2 == 0xFF)
+                {
+                    tagetEncoding = Encoding.BigEndianUnicode;
+                }
+                if (byte1 == 0xff && byte2 == 0xfe && byte3 != 0xff)
+                {
+                    tagetEncoding = Encoding.Unicode;
+                }
+                if (byte1 == 0xef && byte2 == 0xbb && byte3 == 0xbf)
+                {
+                    tagetEncoding = Encoding.UTF8;
+                }
+                stream.Seek(origPos, SeekOrigin.Begin);
+            }
+            return tagetEncoding;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog filedialog = new OpenFileDialog();
+            string FileName = "";
+            if (filedialog.ShowDialog() == DialogResult.OK)
+            {
+                FileName = filedialog.FileName;
+                textBox1.Text = FileName;
+            }
+        }
+    }
+}
